08f003faf4715cb2bc240276aaa6febe79acec82
Daf <afahd.ali@gmail.com>
2019-11-25 02:08:11 +0100

    Added Create and Edit methods in GroupController
    Added the possibility to add and create a Group if needed => the UI is still ugly AF might need to use bootstrap afterwards

---
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/Models/GroupViewModel.cs
@@ -0,0 +1,8 @@
+namespace CodingMilitia.PlayBall.GroupManagement.Web.Models
+{
+    public class GroupViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
